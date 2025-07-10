bd1ecbc315d55eb1b42c6317263b9bfca9ecd305
Oniyemmy24 <Oniyemmy24@gmail.com>
2025-07-10 14:46:05 +0100

    server.js

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
-require('dotenv').config(); 
 const express = require('express');
 
 const mongodb = require('./data/database');
-
-const { MongoClient } = require('mongodb');
-
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
-
 
 mongodb.initDb((err) => {
     if(err) {
