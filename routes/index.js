68999f275a298b5960ad6b80642aed129840817c
Oniyemmy24 <Oniyemmy24@gmail.com>
2025-07-13 01:05:45 +0100

    index.js added

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {res.send('Hello world');});
+//router.get('/', (req, res) => {res.send('Hello world');});
+
+router.use('/users', require('./users'));
 
 module.exports = router;
 
