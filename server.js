68b13edc050eb50342b29798aa9b61f9b7c9509d
Oniyemmy24 <Oniyemmy24@gmail.com>
2025-07-17 17:01:52 +0100

    server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const mongodb = require('./data/database');
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use('/', require('./routes'));
 
