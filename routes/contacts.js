d27873b2473137a0091f8f1ae1d894533af804fc
Oniyemmy24 <Oniyemmy24@gmail.com>
2025-07-22 00:49:16 +0100

    contacts.js

---
+++ routes/contacts.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const contacts = require('../controllers/contacts');
+
+router.get('/', contacts.getAllContacts);
+router.get('/:id', contacts.getSingle);
+router.post('/', contacts.createContact);
+router.put('/:id', contacts.updateContact);
+router.delete('/:id', contacts.deleteContact);
+
+module.exports = router;
