6061648bf710ff1a30a2fb700f6a01158d25da0d
Oniyemmy24 <Oniyemmy24@gmail.com>
2025-07-22 00:51:42 +0100

    swagger.js

---
+++ swagger/swagger.js
@@ -0,0 +1,12 @@
+const swaggerAutogen = require('swagger-autogen')();
+const doc = {
+  info: {
+    title: 'Users Api'
+  },
+  host: 'localhost:3002',
+  schemes: ['https', 'http']
+};
+const outputFile = './swagger.json';
+const endpointsFiles= ['./routes/index..js'];
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
