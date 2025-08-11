f0e624e69e82e7b631c120c22d2760838aec9d9f
Oniyemmy24 <Oniyemmy24@gmail.com>
2025-08-12 00:38:54 +0100

    server.js

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./data/database');
+const cors = require('cors');
 const app = express();
 
+app.use(express.json());
 const port = process.env.PORT || 3002;
 
 app.use((req, res, next) => {
@@ -12,12 +14,20 @@ app.use((req, res, next) => {
         'Access-Control-Allow-Method', 
         'Origin, X-Requested-With, Content-Type, Accept, Z-key'
     );
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     next();
 });
 
-app.use(bodyParser.json());
-app.use('/', require('./routes'));
+app.use(cors({ methods: ["GET", "POST", "DELETE", "UPDATE", "PUT"] }));
+app.use(cors({ origin: "*" }));
+app.use("/", require("./routes/index.js"));
+
+process.on("uncaughtException", (err, origin) => {
+  console.log(
+    process.stderr.fd,
+    `Caught exception: ${err}\n` + `Exception origin: ${origin}`
+  );
+});
 
 mongodb.initDb((err) => {
     if(err) {
