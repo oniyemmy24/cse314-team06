0ba6db269ada68e21315dbc1778ed0d51f404b65
Oniyemmy24 <Oniyemmy24@gmail.com>
2025-08-12 00:44:11 +0100

    index.js

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
+const express = require("express");
 const router = require('express').Router();
 
 router.use('/', require('./swagger'));
 
 router.get('/', (req, res) => {
     //#swagger.tags=['Hello World']
-    res.send('Hello world');
+    res.send('Hello world at week05');
 });
 
 router.use('/users', require('./users'));
+router.use('/devices', require('./devices'));
 
 module.exports = router;
 
